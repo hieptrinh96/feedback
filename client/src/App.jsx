b8bec48f2ba0438caf11bd6d3154b620a77792bb
hieptrinh96 <xmikecolx@gmail.com>
2023-07-11 08:48:29 -0500

    remove old react code and replace it with a blank template

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,9 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
-// import { useState, useEffect } from 'react';
-// import GoogleLoginPage from './pages/GoogleLoginPage/GoogleLoginPage';
-import Auth from './pages/Auth/Auth';
-import {GoogleLogin, useGoogleLogin} from '@react-oauth/google'
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import GoogleLoginPage from './pages/GoogleLoginPage/GoogleLoginPage';
-function App() {
-  const [userName, setUserName] = useState('')
-  const [userEmail, setUserEmail] = useState('')
 
+function App() {
   return (
-    <div>
-      {console.log(userName)}
-        <div>hello</div>
-      <GoogleLogin 
-        onSuccess={credentialResponse => {
-          console.log(credentialResponse)
-        }}
-        theme='filled_blue'
-      />
-      <Routes>
-        <Route 
-          path='/' 
-          element={<GoogleLoginPage />}/>
-          {/* <Route 
-            path='/'
-            element={<Auth />}
-          /> */}
-      </Routes>
+    <div className="App">
+      <h1>hello</h1>
     </div>
   );
 }
--- client/src/services/authService.js
+++
@@ -1,3 +0,0 @@
-import * as tokenService from '../services/tokenServices'
-const url = process.env.REACT_APP_BACK_END_SERVER_URL
-
