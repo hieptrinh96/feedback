03e6761987a5b54c2dfe20023bfffc6e99ea6f2a
hieptrinh96 <xmikecolx@gmail.com>
2023-07-24 16:43:03 -0500

    create new function to add users into database

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -2,9 +2,10 @@ import db from '../db/db.js'
 import XLSX from 'xlsx'
 const showUser = (req, res) => {
   const user = req.user
-  if (user.email.includes('atx')) {
-    
-  }
+  const listOfPersonnels = readExcelFile()
+  // if (user.email.includes('mil')) {
+  //   db.query('update users set ')
+  // }
 }
 
 const readExcelFile = () => {
@@ -13,10 +14,30 @@ const readExcelFile = () => {
   const personnel = tabs.SheetNames[0]
   const sheet = tabs.Sheets[personnel]
   const listOfPersonnels = XLSX.utils.sheet_to_json(sheet)
+  // for (let i = 0; i < listOfPersonnels.length; i++) {
+  //   const person = listOfPersonnels[i]
+  //   console.log(person)
+  // }
   return listOfPersonnels
 }
 
-  readExcelFile()
+const recordUserData = (req, res) => {
+  const user = req.user
+  const listOFUsers = readExcelFile()
+  for (let i = 0; i < listOFUsers.length; i++) {
+    const userInfo = listOFUsers[i]
+    if (user.email.includes('mil')) {
+      if (user.email === user.displayName) {
+        db.query(`update users set Name = ?, Cohort = ?, Current_Team = ?, SDU = ? WHERE Email = ${user.email}`, [userInfo['Name'], userInfo['Cohort'], userInfo['Current Team'], userInfo['SDU']], (err, result) => {
+          if (err) console.log(err)
+          console.log(`${user.displayName} was added`)
+        })
+      }
+    }
+  }
+}
+
+recordUserData()
 
 export {
   showUser,
