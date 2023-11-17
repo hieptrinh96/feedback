ff4cec41645b684226ada33569478a6426db6431
hieptrinh96 <xmikecolx@gmail.com>
2023-11-17 17:17:39 -0600

    Comment out oauth2 and implement form

--- client/src/pages/LandingForm/LandingForm.jsx
+++ client/src/pages/LandingForm/LandingForm.jsx
@@ -1,23 +1,29 @@
 import './LandingFormCSS.css'
-import { useState, useEffect } from 'react';
+import Profile from '../Profile';
+import * as profileService from '../../services/profileService'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const LandingForm = () => {
-  const [email, setEmail] = useState('')
-
-  const handleSubmit = (e) => {
+  const [email, setEmail] = useState([])
+  const navigate = useNavigate()
+  const handleSubmit = async (e) => {
     e.preventDefault()
     // this is where you put the function to look at email
+    const emailData = e.target.email.value
+    const data = await profileService.fetchTeam(emailData)
+    setEmail(data)
+    navigate('/profile', {state: {data}})
   }
   return (
     <div className='inputContainer'>
       <h2>Hello</h2>
-      <form onSubmit={"name of function"}>
+      <form onSubmit={handleSubmit}>
         <label>
           Please enter your email: 
           <input 
             name="email"
-            type='text'
-            // onChange={(event) => setEmail(event.target.value)}
+            type='email'
           />
           <button type='submit'>Enter</button>
         </label>
