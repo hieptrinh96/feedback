9d616ef461d751567411b0cef837fe6f79052e6f
hieptrinh96 <xmikecolx@gmail.com>
2023-11-17 13:37:50 -0600

    Set up component for function

--- client/src/pages/LandingForm/LandingForm.jsx
+++ client/src/pages/LandingForm/LandingForm.jsx
@@ -1,17 +1,29 @@
-
 import './LandingFormCSS.css'
+import { useState, useEffect } from 'react';
+
 const LandingForm = () => {
+  const [email, setEmail] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // this is where you put the function to look at email
+  }
   return (
     <div className='inputContainer'>
       <h2>Hello</h2>
-      <form action="">
+      <form onSubmit={"name of function"}>
         <label>
-          Please enter your email: <input name="name"/>
+          Please enter your email: 
+          <input 
+            name="email"
+            type='text'
+            // onChange={(event) => setEmail(event.target.value)}
+          />
           <button type='submit'>Enter</button>
-          </label>
+        </label>
       </form>
     </div>
   );
 }
- 
+
 export default LandingForm;
